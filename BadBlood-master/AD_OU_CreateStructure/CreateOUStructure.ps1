e3e6629dde25eb04f379b309c5b06932b4bcfde6
Jamie.Lee <jamie.tlee@outlook.com>
2022-08-15 14:59:15 +0100

    update create ou's

--- BadBlood-master/AD_OU_CreateStructure/CreateOUStructure.ps1
+++ BadBlood-master/AD_OU_CreateStructure/CreateOUStructure.ps1
@@ -5,16 +5,16 @@ function Get-ScriptDirectory {
 }
 $scriptPath = Get-ScriptDirectory
 
-$TopLevelOUs = @('Admin', 'Tier 1','Tier 2','Stage', 'Quarantine', 'Grouper-Groups', 'People','Testing','.SecFrame.com')
+$TopLevelOUs = @('IT Operations', 'IT Helpdesk','Finance','Marketing', 'Human Resources', 'FOF')
     
-$AdminSubOUs = @('Tier 0', 'Tier 1', 'Tier 2', 'Staging') 
+$AdminSubOUs = @() 
     #loop before the ou name by making T#-OBJECT name as the OU
-$AdminobjectOUs = @('Accounts', 'Servers', 'Devices', 'Permissions','Roles') 
+$AdminobjectOUs = @() 
 #########################
-$skipSubOUs = @('Deprovision', 'Quarantine', 'Groups')
+$skipSubOUs = @('IT Operations', 'IT Helpdesk','Finance','Marketing', 'Human Resources', 'FOF')
 #########################
 #$tierOUs = @('Tier 1', 'Tier 2')
-$ObjectSubOUs = @('ServiceAccounts', 'Groups', 'Devices','Test')
+$ObjectSubOUs = @()
 
 
 #Consodated list of all 3 letter codes which IAM uses. 
@@ -64,38 +64,38 @@ foreach ($name in $TopLevelOUs) {
     elseif ($skipSubOUs -contains $name) {
         #this skips the creation of the sub containers
     }
-    elseif (($name -eq 'Tier 1') -or ($name -eq 'Tier 2') -or ($name -eq 'Stage')) {
-        $fulldn = "OU=" + $name + "," + $dn 
-        $csvlist = @()
-        $csvlist = import-csv $3LetterCodeCSV
+    # elseif (($name -eq 'Tier 1') -or ($name -eq 'Tier 2') -or ($name -eq 'Stage')) {
+    #     $fulldn = "OU=" + $name + "," + $dn 
+    #     $csvlist = @()
+    #     $csvlist = import-csv $3LetterCodeCSV
 
-        foreach ($ou in $csvlist) {
-            New-ADOrganizationalUnit -Name ($ou.name) -Path $fulldn -Description ($ou.description)
-            $csvdn = "OU=" + $ou.name + "," + $fulldn 
+    #     foreach ($ou in $csvlist) {
+    #         New-ADOrganizationalUnit -Name ($ou.name) -Path $fulldn -Description ($ou.description)
+    #         $csvdn = "OU=" + $ou.name + "," + $fulldn 
             
-            foreach ($ObjectSubOU in $ObjectSubOUs) {
-                New-ADOrganizationalUnit -Name $ObjectSubOU -Path $csvdn
-                $Objectfulldn = "OU=" + $ObjectSubOU + "," + $csvdn
-            }
-        }
-    }
+    #         foreach ($ObjectSubOU in $ObjectSubOUs) {
+    #             New-ADOrganizationalUnit -Name $ObjectSubOU -Path $csvdn
+    #             $Objectfulldn = "OU=" + $ObjectSubOU + "," + $csvdn
+    #         }
+    #     }
+    # }
 
-    elseif (($name -eq 'People')) {
-        $fulldn = "OU=" + $name + "," + $dn 
-        $csvlist = @()
-        $csvlist = import-csv $3LetterCodeCSV
+    # elseif (($name -eq 'People')) {
+    #     $fulldn = "OU=" + $name + "," + $dn 
+    #     $csvlist = @()
+    #     $csvlist = import-csv $3LetterCodeCSV
         
 
 
-        foreach ($ou in $csvlist) {
-            New-ADOrganizationalUnit -Name ($ou.name) -Path $fulldn -Description ($ou.description)
-            $csvdn = "OU=" + $ou.name + "," + $fulldn 
+    #     foreach ($ou in $csvlist) {
+    #         New-ADOrganizationalUnit -Name ($ou.name) -Path $fulldn -Description ($ou.description)
+    #         $csvdn = "OU=" + $ou.name + "," + $fulldn 
             
-        }
-        #Create Two Sub OUs in People OU required for IDM provisioning 
-        New-ADOrganizationalUnit -Name 'Deprovisioned' -Path $fulldn -Description 'User account that have been deprovisioned by the IDM System'
-        New-ADOrganizationalUnit -Name 'Unassociated' -Path $fulldn -Description 'User Object that do have have any department affliation'
-    }
+    #     }
+    #     #Create Two Sub OUs in People OU required for IDM provisioning 
+    #     New-ADOrganizationalUnit -Name 'Deprovisioned' -Path $fulldn -Description 'User account that have been deprovisioned by the IDM System'
+    #     New-ADOrganizationalUnit -Name 'Unassociated' -Path $fulldn -Description 'User Object that do have have any department affliation'
+    # }
     
     else {}
     $x++
